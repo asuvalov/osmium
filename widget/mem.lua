58ea14cb79e37a0f183cb2ade431cda4b2ca5a5f
a.suvalov <suvalov.andrew@gmail.com>
2020-04-19 00:40:19 +0300

    small updates

--- widget/mem.lua
+++ widget/mem.lua
@@ -9,42 +9,45 @@ local wibox                = require("wibox")
 local naughty              = require("naughty")
 local tconcat              = table.concat
 local gmatch, lines, floor = string.gmatch, io.lines, math.floor
+local fmt = string.format
 
 -- Memory usage (ignoring caches)
 -- lain.widget.mem
 
 local function factory(args)
-    local mem       = { widget = wibox.widget.textbox() }
-    local args      = args or {}
-    local timeout   = args.timeout or 2
-    local settings  = args.settings or function() end
-    local showpopup = args.showpopup or "on"
+  local mem       = { widget = wibox.widget.textbox() }
+  local args      = args or {}
+  local timeout   = args.timeout or 2
+  local settings  = args.settings or function() end
+  local showpopup = args.showpopup or "on"
 
-    mem.followtag           = args.followtag or false
-    mem.notification_preset = args.notification_preset
+  mem.followtag           = args.followtag or false
+  mem.notification_preset = args.notification_preset
 
-    if not mem.notification_preset then
-        mem.notification_preset = {
-            font = "Monospace 10",
-            fg   = "#FFFFFF",
-            bg   = "#000000"
-        }
+  if not mem.notification_preset then
+      mem.notification_preset = {
+        font = "Monospace 10",
+        fg   = "#FFFFFF",
+        bg   = "#000000"
+      }
+  end
+
+  function mem.hide()
+    if not mem.notification then
+      return
     end
+    naughty.destroy(mem.notification)
+    mem.notification = nil
+  end
 
-    function mem.hide()
-        if not mem.notification then return end
-        naughty.destroy(mem.notification)
-        mem.notification = nil
-    end
-
-    function mem.show(seconds, scr)
-        mem.hide(); mem.update()
-        mem.notification_preset.screen = mem.followtag and focused() or scr or 1
-        mem.notification = naughty.notify {
-            preset  = mem.notification_preset,
-            timeout = type(seconds) == "number" and seconds or 5
-        }
-    end
+  function mem.show(seconds, scr)
+    mem.hide(); mem.update()
+    mem.notification_preset.screen = mem.followtag and focused() or scr or 1
+    mem.notification = naughty.notify {
+      preset  = mem.notification_preset,
+      timeout = type(seconds) == "number" and seconds or 5
+    }
+  end
 
   function mem.update()
       mem_now = {}
@@ -61,17 +64,17 @@ local function factory(args)
           end
       end
 
-    mem_now.used = mem_now.total - mem_now.free - mem_now.buf - mem_now.cache - mem_now.srec
+    mem_now.used     = mem_now.total - mem_now.free - mem_now.buf - mem_now.cache - mem_now.srec
     mem_now.swapused = floor(mem_now.swap - mem_now.swapf)
-    mem_now.perc = math.floor(mem_now.used / mem_now.total * 100)
+    mem_now.perc     = floor(mem_now.used / mem_now.total * 100)
 
     widget = mem.widget
     settings()
 
-    local notifytable = { [1]   = string.format("%-5s\t%-5s\t%-5s\t%-5s\t%-4s\n", "type", "total", "used", "free", "unit") }
-    notifytable[#notifytable+1] = string.format("%-5s\t%-5d\t%-5d\t%-5d\t%-4s\n", "ram", mem_now.total, mem_now.used, mem_now.free,  "MB")
-    notifytable[#notifytable+1] = string.format("%-5s\t%-5d\t%-5d\t\t%-5d\t%-4s", "swap", mem_now.swap, mem_now.swapused, mem_now.swapf, "MB")
-    mem.notification_preset.text = tconcat(notifytable) 
+    local text = { [1] = fmt("%-5s\t%-5s\t%-5s\t%-5s\t%-4s\n", "type", "total", "used", "free", "unit") }
+    text[#text+1]      = fmt("%-5s\t%-5d\t%-5d\t%-5d\t%-4s\n", "ram",  mem_now.total, mem_now.used, mem_now.free,  "MB")
+    text[#text+1]      = fmt("%-5s\t%-5d\t%-5d\t%-5d\t%-4s"  , "swap", mem_now.swap, mem_now.swapused, mem_now.swapf, "MB")
+    mem.notification_preset.text = table.concat(text)
   end
 
   if showpopup == "on" then
